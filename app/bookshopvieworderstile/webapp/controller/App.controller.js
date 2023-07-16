41dcda7d17568111dd788825a77dc2d20fffc5a6
Bart VERHOEVEN <verhoeba>
2023-07-16 21:03:47 +0000

    change tile look and behaviour

--- app/bookshopvieworderstile/webapp/controller/App.controller.js
+++ app/bookshopvieworderstile/webapp/controller/App.controller.js
@@ -7,6 +7,17 @@ sap.ui.define(
   
       return BaseController.extend("com.danone.bookshopvieworderstile.controller.App", {
         onInit() {
+        },
+
+        async onPress() {
+          let oCrossAppNavigator = await sap.ushell.Container.getServiceAsync("CrossApplicationNavigation");
+          oCrossAppNavigator.toExternal({
+            target: {
+              semanticObject: "Orders",
+              action: "Display"
+            },
+            params: {}
+          });
         }
       });
     }
